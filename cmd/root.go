6f2ebadc8ac0e5fb574913a57b6114fef2f788f2
mini-blynko <bryan@birdseyeglobal.com>
2026-07-14 00:33:29 -0400

    init project

---
+++ cmd/root.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "th",
+	Short: "Treehouse a Git Worktree manager to make your life easier",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Blynkosaur/treehouse/cmd"
+
+func main(){
+	cmd.Execute()
+}
