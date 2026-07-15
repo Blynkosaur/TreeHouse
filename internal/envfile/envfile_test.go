9f4a8083334bfd6ea69b45d35c37d021ee0a218f
mini-bryanlin <bryanlin316@gmail.com>
2026-07-14 21:54:56 -0400

    tests: unit tests for env parser

--- internal/envfile/envfile_test.go
+++ internal/envfile/envfile_test.go
@@ -1,6 +1,54 @@
 package envfile
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+type envVariableItem struct {
+	name  string
+	input string
+	want  map[string]string
+}
+
+var testItems = []envVariableItem{
+	{
+		name:  "simple pair",
+		input: "FOO=bar",
+		want:  map[string]string{"FOO": "bar"},
+	},
+	{
+		name:  "comments and blank lines skipped",
+		input: "# database config\n\nFOO=bar\n",
+		want:  map[string]string{"FOO": "bar"},
+	},
+	{
+		name:  "empty value is present but empty",
+		input: "REDIS_URL=",
+		want:  map[string]string{"REDIS_URL": ""},
+	},
+	{
+		name:  "quoted value has quotes stripped",
+		input: `JWT_SECRET="abc123"`,
+		want:  map[string]string{"JWT_SECRET": "abc123"},
+	},
+	{
+		name:  "garbage line without equals is skipped",
+		input: "this is not a pair\nFOO=bar",
+		want:  map[string]string{"FOO": "bar"},
+	},
+}
 
 func TestParse(t *testing.T) {
+	for _, test := range testItems {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := Parse(test.input)
+			if err != nil {
+				t.Fatal("Parse() returned idk something wrong")
+			}
+			if !reflect.DeepEqual(res, test.want) {
+				t.Errorf("Parse(%q)\n  got:  %#v\n  want: %#v", test.input, res, test.want)
+			}
+		})
+	}
 }
